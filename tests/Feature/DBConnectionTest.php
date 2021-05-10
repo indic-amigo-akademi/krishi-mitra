7156cebd657c2ef59f99971b4fe6cfbe298c9ab5
Andre1998Shuvam <shuvamghosal98@gmail.com>
2021-05-10 15:24:08 +0530

    Added Database Connection Testing Script

---
+++ tests/Feature/DBConnectionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class DBConnectionTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $pdo = DB::connection('mysql')->getPdo();
+        $this->assertNotNull($pdo);
+    }
+}
+

53c9883c9d05a34fcad3e4b68041f0b3206c2ada
karmakarsrija <karmakarsrija2@gmail.com>
2021-06-08 21:15:58 +0530

    Update RegisterTest.php

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -48,4 +48,50 @@ class RegisterTest extends TestCase
         
     }
     
+    
+    
+    public function testEmailIsNotString() {
+		$data = [
+			'name' => 'Test',
+            'username' => 'Test123',
+            'phonenumber'=>'1234567890',
+            'email'=>'123',
+            'password' => 'secret1234',
+            'password_confirmation' => 'secret1234',
+		];
+       $response = $this->json('POST',route('user.register.validate'),$data);
+       $response->assertStatus(500);
+       //$this->assertJson(['email']);
+     }
+     public function test_password_should_match_with_confirmpassword() {
+		$data = [
+			'name' => 'Test',
+            'username' => 'Test123',
+            'phonenumber'=>'1234567890',
+            'email'=>'test@123',
+            'password' => 'secret1234',
+            'password_confirmation' => 'ancdef1234',
+		];
+       $response = $this->json('POST',route('user.register.validate'),$data);
+       $this->withoutMiddleware();
+      $response->assertStatus(200);
+       //$this->assertJson(['email']);
+     }
+
+     public function test_mobile_should_be_10_number() {
+		$data = [
+			'name' => 'Test',
+            'username' => 'Test123',
+            'phonenumber'=>'123456',
+            'email'=>'test@123',
+            'password' => 'secret1234',
+            'password_confirmation' => 'ancdef1234',
+		];
+       $response = $this->json('POST',route('user.register.validate'),$data);
+       $this->withoutMiddleware();
+      $response->assertStatus(200);
+     }
+     
+   //test case pass
+    
     }

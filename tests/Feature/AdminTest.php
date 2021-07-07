cd88ca6f55b3bea282e45fed2f9ae0026c18b11e
Andre1998Shuvam <shuvamghosal98@gmail.com>
2021-07-07 14:37:46 +0530

    Updated AdminTest script

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -26,74 +26,69 @@ class AdminTest extends TestCase
         $this->sysadmin = User::where("id", "=", 1)->first();
         $this->admin = User::where("id", "=", 2)->first();
         $this->seller = User::where("id", "=", 11)->first();
-        $this->customer1 = User::where("id", "=", 20)->first();
-        $this->customer2 = User::where("id", "=", 21)->first();
-        $this->dummy_customer = User::where("id", "=", 8)->first();
+        $this->customer = User::where("id", "=", 8)->first();
         $this->setUpFaker();
-        // $this->faker->seed(1235);
     }
 
     public function test_admin_register()
     {
-        $data1 = [
-            'email' => $this->customer1->email,
+        $data = [
+            'email' => $this->customer->email,
             'password' => 'Mohan@1999'
         ];
-
-        $data2 = [
-            'email' => $this->customer2->email,
-            'password' => 'Mohan@1999'
-        ];
-
-        $response1 = $this->actingAs($this->customer1)->post(route('admin.register'), $data1);
-
-        Auth::logout();
-        $response2 = $this->actingAs($this->customer2)->post(route('admin.register'), $data2);
-        $lastApproval1 = Approval::all()->where('type', '=', 'admin_approval')->sortByDesc('id')->first();
-        $lastApproval2 = Approval::all()->where('type', '=', 'admin_approval')->sortBy('id')->first();
-        $this->assertEquals($lastApproval1->type, 'admin_approval');
-        $this->assertEquals($lastApproval2->type, 'admin_approval');
-        $this->assertEquals($lastApproval1->user_id, $this->customer2->id);
-        $this->assertEquals($lastApproval2->user_id, $this->customer1->id);
+        $response1 = $this->actingAs($this->customer)->post(route('admin.register'), $data);
+        $lastApproval = Approval::all()->sortByDesc('updated_at')->first();
+        $this->assertEquals($lastApproval->type, 'admin_approval');
+        $this->assertEquals($lastApproval->user_id, $this->customer->id);
     }
 
     public function test_approval()
     {
-        $lastApproval1 = Approval::all()->where('type', '=', 'admin_approval')->sortByDesc('id')->first();
-        $lastApproval2 = Approval::all()->where('type', '=', 'admin_approval')->sortBy('id')->first();
-
-        $seller_data = [
-            'user_id' => $this->dummy_customer->id,
+        $lastApproval1 = Approval::all()->sortByDesc('updated_at')->first();
+        $approve_data = ['input' => 'approve', 'id' => $lastApproval1->id];
+        $admin_obj = [
+            'user_id' => $this->customer->id,
+            'type' => 'admin_approval',
+        ];
+        $seller_obj = [
+            'user_id' => $this->customer->id,
             'type' => 'seller_approval',
         ];
-        $sellerApproval = Approval::create($seller_data);
 
-        $data1 = ['input' => 'approve', 'id' => $lastApproval2->id];
-        $data2 = ['input' => 'decline', 'id' => $lastApproval1->id];
-        $seller_data = ['input' => 'approve', 'id' => $sellerApproval->id];
-        $response1 = $this->actingAs($this->admin)->post(route('admin.approval'), $data1);
-        $response2 = $this->actingAs($this->admin)->post(route('admin.approval'), $data2);
-        $response3 = $this->actingAs($this->admin)->post(route('admin.approval'), $seller_data);
-
+        $response1 = $this->actingAs($this->admin)->post(route('admin.approval'), $approve_data);
         $c1 = User::all()
             ->where('id', '=', $lastApproval1->user_id)->first();
+        $this->assertEquals($c1->role, 'admin');
+        $c1->role = 'customer';
+        $c1->save();
+
+        $declineApproval = Approval::create($admin_obj);
+        $decline_data = ['input' => 'decline', 'id' => $declineApproval->id];
+        $response2 = $this->actingAs($this->admin)->post(route('admin.approval'), $decline_data);
 
         $c2 = User::all()
-            ->where('id', '=', $lastApproval2->user_id)->first();
+            ->where('id', '=', $declineApproval->user_id)->first();
+        $this->assertEquals($c2->role, 'customer');
+        $c2->role = 'customer';
+        $c2->save();
+
+        $req = [
+            'name' => 'TestSeller',
+            'gstin' => '6512r65',
+            'aadhaar' => '333311114444',
+            'trade_name' => 'UnitTestingSeller'
+        ];
+        $response = $this->actingAs($this->customer)->post(route('seller.create'), $req);
+        $sellerApproval = Approval::all()->sortByDesc('updated_at')->first();
+        $seller_data = ['input' => 'approve', 'id' => $sellerApproval->id];
+        $response3 = $this->actingAs($this->admin)->post(route('admin.approval'), $seller_data);
 
         $seller = User::all()
             ->where('id', '=', $sellerApproval->user_id)->first();
-
-        $this->assertEquals($c1->role, 'customer');
-        $this->assertEquals($c2->role, 'admin');
         $this->assertEquals($seller->role, 'seller');
-
-        $c1->role = 'customer';
-        $c2->role = 'customer';
         $seller->role = 'customer';
-        $c1->save();
-        $c2->save();
         $seller->save();
+
         $response1->assertStatus(302);
         $response1->assertRedirect(route('admin.approval.view'));
 
